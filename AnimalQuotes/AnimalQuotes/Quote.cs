a335cfe6b0025c5b35faf7ad6d89d561938d283e
alex schubert <aschubert254@gmail.com>
2017-12-31 16:50:40 -0700

    Initial Entry

---
+++ AnimalQuotes/AnimalQuotes/Quote.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalQuotes
+{
+    class Quote
+    {
+        private String text;
+        private String author;
+        private String attribute;
+
+        
+
+        
+        //Quote structure
+        public Quote(String text, String author)
+            {
+                this.text = text;
+                this.author = author;
+                
+            }
+
+        public string Text { get => text; set => text = value; }
+        public string Author { get => author; set => author = value; }
+       
+
+    }
+
+    
+       
+   
+      
+}
---
+++ AnimalQuotes/AnimalQuotes/QuoteLib.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalQuotes
+{
+    //Master library for quotes
+    class QuoteLib
+    {
+        private static Quote[] alcoholQuoteLib;
+        private static Quote[] loveQuoteLib;
+        public QuoteLib()
+        {
+            //Quotes about alcohol
+            alcoholQuoteLib = new Quote[]
+    {
+
+           new Quote("Always do sober what you said you’d do drunk.That will teach you to keep your mouth shut." , "Ernest Hemingway"),
+           new Quote("“A drunk man’s words are a sober man’s thoughts." ,"Steve Fergosi"),
+           new Quote("Beer is proof that God loves us and wants us to be happy.", "Benjamin Franklin"),
+           new Quote("Here’s to alcohol, the cause of, and solution to, all life’s problems.", "Homer Simpson"),
+           new Quote("Alcohol may be man’s worst enemy, but the bible says love your enemy.", "Frank Sinatra"),
+           new Quote("Sometimes too much to drink is barely enough.", "Mark Twain"),
+           new Quote("irst you take a drink, then the drink takes a drink, then the drink takes you.", "F. Scott Fitzgerald"),
+           new Quote("Come, gentlemen, I hope we shall drink down all unkindness.", "William Shakespeare"),
+           new Quote("Worthless people live only to eat and drink; people of worth eat and drink only to live.","Socrates"),
+           new Quote("Some people drink from the fountain of knowledge, others just gargle.", "Robert Anthony")
+
+   };
+        }
+
+    }
+}
